defce065b21a1d41ea3170a646823ae5d0d28d37
akshatsingh1427 <singhakshatamlesh@gmail.com>
2026-07-06 08:35:49 +0530

    Refactor train.py for improved readability

--- train.py
+++ train.py
@@ -21,7 +21,6 @@ images = np.expand_dims(images, -1)  # Add channel dimension
 
 labels = keras.utils.to_categorical(emotions, 7)
 
-# Split data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
